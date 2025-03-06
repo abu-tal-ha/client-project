719431571380b4fa1164bcae69ab1ec8785fc554
abu-tal-ha <mabutalha468@gmail.com>
2025-03-06 17:21:38 +0600

    card click

---
+++ assets/js/custom.js
@@ -0,0 +1,13 @@
+// function expandImage(element) {
+//     element.classList.toggle("fullscreen"); // Toggle করলে বড়/ছোট হবে
+// }
+function openFullscreen(imageSrc) {
+    let modal = document.getElementById("imageModal");
+    let fullImage = document.getElementById("fullImage");
+    fullImage.src = imageSrc;
+    modal.style.display = "block";
+}
+
+function closeFullscreen() {
+    document.getElementById("imageModal").style.display = "none";
+}
