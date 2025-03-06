a8669be3006d4a88ce911405199bee5b46f81140
abu-tal-ha <mabutalha468@gmail.com>
2025-03-07 02:21:00 +0600

    client code completed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,35 @@
 // function expandImage(element) {
 //     element.classList.toggle("fullscreen"); // Toggle করলে বড়/ছোট হবে
 // }
+// function openFullscreen(imageSrc) {
+//     let modal = document.getElementById("imageModal");
+//     let fullImage = document.getElementById("fullImage");
+//     fullImage.src = imageSrc;
+//     modal.style.display = "block";
+// }
+
+// function closeFullscreen() {
+//     document.getElementById("imageModal").style.display = "none";
+// }
+
+// function openFullscreen(imageSrc) {
+//     const modal = document.getElementById("imageModal");
+//     const fullImage = document.getElementById("fullImage");
+//     fullImage.src = imageSrc;
+//     modal.style.display = "block";
+//   }
+
+//   function closeFullscreen() {
+//     document.getElementById("imageModal").style.display = "none";
+//   }
+
+
+
 function openFullscreen(imageSrc) {
-    let modal = document.getElementById("imageModal");
-    let fullImage = document.getElementById("fullImage");
-    fullImage.src = imageSrc;
-    modal.style.display = "block";
+  document.getElementById("fullImage").src = imageSrc;
+  document.getElementById("imageModal").style.display = "block";
 }
 
 function closeFullscreen() {
-    document.getElementById("imageModal").style.display = "none";
+  document.getElementById("imageModal").style.display = "none";
 }
