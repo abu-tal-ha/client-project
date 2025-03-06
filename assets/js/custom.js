c601d5cdd3ccb1e89c66ed3226fad97985d10ee4
abu-tal-ha <mabutalha468@gmail.com>
2025-03-07 05:41:18 +0600

    form reset code updated

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,30 +1,3 @@
-// function expandImage(element) {
-//     element.classList.toggle("fullscreen"); // Toggle করলে বড়/ছোট হবে
-// }
-// function openFullscreen(imageSrc) {
-//     let modal = document.getElementById("imageModal");
-//     let fullImage = document.getElementById("fullImage");
-//     fullImage.src = imageSrc;
-//     modal.style.display = "block";
-// }
-
-// function closeFullscreen() {
-//     document.getElementById("imageModal").style.display = "none";
-// }
-
-// function openFullscreen(imageSrc) {
-//     const modal = document.getElementById("imageModal");
-//     const fullImage = document.getElementById("fullImage");
-//     fullImage.src = imageSrc;
-//     modal.style.display = "block";
-//   }
-
-//   function closeFullscreen() {
-//     document.getElementById("imageModal").style.display = "none";
-//   }
-
-
-
 function openFullscreen(imageSrc) {
   document.getElementById("fullImage").src = imageSrc;
   document.getElementById("imageModal").style.display = "block";
@@ -33,3 +6,11 @@ function openFullscreen(imageSrc) {
 function closeFullscreen() {
   document.getElementById("imageModal").style.display = "none";
 }
+
+
+
+document.getElementById("myForm").addEventListener("submit", function (event) {
+  setTimeout(() => {
+    event.target.reset(); 
+  }, 1000); 
+});
